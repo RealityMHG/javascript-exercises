7b2938ce9dc828c60c9c0eab5779ebf9f41e0b03
RealityMHG <rafaelrego2000@gmail.com>
2024-06-09 22:50:53 +0100

    Finished exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,12 @@
-const fibonacci = function() {
+const fibonacci = function(fibonacciLength) {
+    if(fibonacciLength<0) return "OOPS"
+    const fibonacciSequence = [0,1];
 
+    for(let i=2; i<fibonacciLength+1; i++) {
+        fibonacciSequence.push(fibonacciSequence[i-1] + fibonacciSequence[i-2]);
+    }
+
+    return fibonacciSequence[fibonacciLength];
 };
 
 // Do not edit below this line
