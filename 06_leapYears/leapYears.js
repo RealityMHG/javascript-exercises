a964dfa5a25e9287aa2794088c3f314e85e02182
RealityMHG <rafaelrego2000@gmail.com>
2024-06-09 18:55:16 +0100

    Finished exercise 6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,5 @@
-const leapYears = function() {
-
+const leapYears = function(yearToTest) {
+    return yearToTest % 4 === 0 && (yearToTest % 100 !== 0 || yearToTest % 400 === 0);
 };
 
 // Do not edit below this line
