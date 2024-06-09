63cfce87a7954dbbc8adceac3222be56b695b492
RealityMHG <rafaelrego2000@gmail.com>
2024-06-09 18:43:49 +0100

    Finished exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,21 @@
-const sumAll = function() {
+const sumAll = function(initialNumber,lastNumber) {
+    if (initialNumber<0 || lastNumber<0 ) return "ERROR";
+
+    if(typeof initialNumber != "number" || typeof lastNumber != "number") return "ERROR";
+
+    let sumOfNumbers = 0;
+
+    if(initialNumber<lastNumber) {
+        for(let i=initialNumber; i<=lastNumber; i++) {
+            sumOfNumbers += i;
+        }
+    } else {
+        for(let i=lastNumber; i<=initialNumber; i++) {
+            sumOfNumbers +=i;
+        }
+    }
+
+    return sumOfNumbers;
 
 };
 
