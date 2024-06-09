180b1d6f7aa4578936dea761fa7605d363397ead
RealityMHG <rafaelrego2000@gmail.com>
2024-06-09 22:43:22 +0100

    Finished exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (palindromeString) {
+    const stringWithoutPonc = palindromeString.replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, '');
+    const stringWihtoutSpace = stringWithoutPonc.replace(/\s+/g, '');
+    const stringLowercase = stringWihtoutSpace.toLowerCase();
+    let reversedString = stringLowercase.split("").reverse().join("");
 
+    if(reversedString===stringLowercase) return true;
+
+    return false;
 };
 
 // Do not edit below this line
