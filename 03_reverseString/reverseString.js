62ba72c7647117b466ed7a1e1f7bd36aff31a738
RealityMHG <rafaelrego2000@gmail.com>
2024-06-09 17:23:43 +0100

    Finished exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(textToReverse) {
+    let reversedText = "";
+    for(let i=textToReverse.length-1; i>=0; i--) {
+        reversedText += textToReverse[i];
+    }
+    return reversedText;
 };
 
 // Do not edit below this line
