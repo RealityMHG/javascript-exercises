8f34b330245a2504a4a4d76cdb1eaf27150b03ea
RealityMHG <rafaelrego2000@gmail.com>
2024-06-09 22:20:17 +0100

    Finished Exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,52 @@
-const add = function() {
-	
+const add = function(firstNumber,secondNumber) {
+	return firstNumber + secondNumber;
 };
 
-const subtract = function() {
-	
+const subtract = function(firstNumber,secondNumber) {
+	return firstNumber - secondNumber;
 };
 
-const sum = function() {
-	
+const sum = function(numberArray) {
+  if(numberArray.length==0) return 0;
+
+	let sumOfNumbers = numberArray[0];
+
+  for(let i=1; i<numberArray.length; i++) {
+    sumOfNumbers += numberArray[i];
+  }
+  return sumOfNumbers;
 };
 
-const multiply = function() {
+const multiply = function(numberArray) {
+  if(numberArray.length==0) return 0;
 
+  let resultOfNumbers = numberArray[0];
+
+  for(let i=1; i<numberArray.length; i++) {
+    resultOfNumbers *= numberArray[i];
+  }
+  return resultOfNumbers;
 };
 
-const power = function() {
-	
+const power = function(firstNumber,powerNumber) {
+	let resultOfPower = 1;
+
+  for(let i=1; i<=powerNumber; i++) {
+    resultOfPower *= firstNumber;
+  }
+  return resultOfPower;
 };
 
-const factorial = function() {
-	
+const factorial = function(factorialNumber) {
+  if (factorialNumber == 0) return 1;
+
+  if(factorialNumber == 1 || factorialNumber == 2) return factorialNumber;
+  
+  let resultOfFactorial = factorialNumber;
+  for(let i=factorialNumber-1; i!=0; i--) {
+    resultOfFactorial *= i;
+  }
+  return resultOfFactorial;
 };
 
 // Do not edit below this line
