710e0fe9fc2aabfd147164f7f7933741d6d84575
RealityMHG <rafaelrego2000@gmail.com>
2024-06-09 17:11:23 +0100

    Finished exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function(textToRepeat, numberOfTimes) {
+    if(numberOfTimes<0) return "ERROR";
 
+    let finalString = "";
+    for(let i=0; i<numberOfTimes; i++) {
+        finalString += textToRepeat;
+    }
+    return finalString;
 };
 
 // Do not edit below this line
