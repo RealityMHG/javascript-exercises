634191902a910486360806cc516f77cdaf3fd073
RealityMHG <rafaelrego2000@gmail.com>
2024-06-09 18:33:15 +0100

    Finished exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function(arrayToChange, numberToRemove) {
+
+    const numbersToRemove = arguments;
+    numbersToRemove.shift;
+
+    let arrayChanged = arrayToChange.filter(function (element) {
+        return element !== numberToRemove;
+    });
+
+    if(numbersToRemove.length >= 2) {
+        for (let i=1; i<numbersToRemove.length; i++) {
+            arrayChanged = arrayChanged.filter(function (element) {
+                return element !== numbersToRemove[i];
+            });
+        }
+    }
+
+    return arrayChanged;
 };
 
 // Do not edit below this line
